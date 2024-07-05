2d13e1a1484443204eb6cec51d0c33d61fd0010f
Supanat34 <6511130034@mut.ac.th>
2024-07-05 16:59:09 +0700

    commit2

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?<php echo "my repository" ?>
+<?<php 
+    echo "<h1>My repository</h1>"
+    echo "<h1>My Name is Nungom</h1> "
+    ?>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>Main</h1>";
+    echo "<h1>Aboutme</h1>";
+    echo "<h1>Map</h1>";
+    ?>
