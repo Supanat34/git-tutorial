ff059ad80427ae4ce274f94866a186d738d8b201
Supanat34 <6511130034@mut.ac.th>
2024-07-05 17:03:47 +0700

    commit3

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?<php 
-    echo "<h1>My repository</h1>"
-    echo "<h1>My Name is Nungom</h1> "
+    echo "<h1>My repository</h1>";
+    echo "<h1>My Name is Nungom</h1>";
+    echo "<h1>Minecraft</h1>";
     ?>
