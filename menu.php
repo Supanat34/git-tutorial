3e5c65ba82f4435269fed40c082bd515d250c2b8
Supanat34 <6511130034@mut.ac.th>
2024-07-05 17:00:59 +0700

    commit3

--- menu.php
+++ menu.php
@@ -1,5 +1,6 @@
 <?php
-    echo "<h1>Main</h1>";
-    echo "<h1>Aboutme</h1>";
-    echo "<h1>Map</h1>";
+    echo "<h1>หน้าหลัก</h1>";
+    echo "<h1>สินค้า</h1>";
+    echo "<h1>เกี่ยวกับเรา</h1>";
+    echo "<h1>แผนที่</h1>";
     ?>
