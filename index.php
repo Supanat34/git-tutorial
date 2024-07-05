94784daf9cc2ec42c565cf2c6fceb1a2be9bd19e
Supanat34 <6511130034@mut.ac.th>
2024-07-05 17:11:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "<h1>My repository</h1>";
     echo "<h1>My Name is Nungom</h1>";
     echo "<h1>Minecraft jv</h1>";
+    echo "<h1>My repository</h1>";
+    echo "<h1>My Name is Nungom</h1>";
+    echo "<h1>Minecraft jv</h1>";
     ?>
