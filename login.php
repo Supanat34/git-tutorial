ccc296b02b45e86e0a76f4c0eb2401cc4a35f0d7
Supanat34 <6511130034@mut.ac.th>
2024-07-12 17:28:56 +0700

    ver2

--- login.php
+++ login.php
@@ -2,10 +2,7 @@
     echo "<h1> <table> </h1>";
     echo "<h1> User: </h1>";
     echo "<h1> Password: </h1>";
-<<<<<<< HEAD
     echo "<h1> </table> </h1>";
     ?>
-=======
     echo "<h1> Mahanakorn: </h1>";
     ?>
->>>>>>> bcc6e13f16950180b83b3742073402019792c58c
